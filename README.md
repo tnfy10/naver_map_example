09247568684ccae49072e3098eb6176a20d72968
Hong Sung-hwi <tnfy10@gmail.com>
2022-01-29 13:01:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# naver_map_example
+
+flutter 네이버 지도 연동 예제
---
+++ android/app/src/main/kotlin/tnfy10/example/naver_map_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package tnfy10.example.naver_map_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
